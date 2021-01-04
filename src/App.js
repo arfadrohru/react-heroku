94eff873d0bbebf8d39c46e9e5af08193c3f95a3
arfadrohru <arfadrohru@gmail.com>
2021-01-04 23:34:44 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>React Heroku</h1>
+        <h1>Cek Commit</h1>
       </header>
     </div>
   );
