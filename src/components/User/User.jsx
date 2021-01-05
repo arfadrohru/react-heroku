0923c917596b376666deab27a4872fd0ab53c1eb
arfadrohru <arfadrohru@gmail.com>
2021-01-05 12:03:16 +0700

    menampilkan data dari database

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -5,20 +5,26 @@ export default class User extends Component {
     constructor(){
         super()
         this.state ={
-            nama : ''
+            user : {}
         }
     }
     componentDidMount(){
-axios.get('https://deploy-springboot-heroku.herokuapp.com/api/user').then(res =>{
+axios.get('https://deploy-springboot-heroku.herokuapp.com/api/read?id=1').then(res =>{
+    let data = res.data
     this.setState({
-        nama: res.data.user
+        user: data.Message
     })
+    console.log(data);
+    console.log(data.Message);
 })
     }
     render() {
         return (
             <div>
-                <h1>Get Name From Heroku Spring Boot : {this.state.nama}</h1>
+                <h1>Get Data From Heroku Spring Boot </h1>
+                <p>name : {this.state.user.name}</p>
+                <p>email : {this.state.user.email}</p>
+                <p>age : {this.state.user.age}</p>
             </div>
         )
     }
